4caa872f03fd9512ed0ba7da1acaffefaff980a6
L1 L31 <wellming.li@howzhi.com>
2021-09-13 01:32:43 +0800

    classroom api

--- api/src/main/java/com/qingketang/graphql/ClassroomController.java
+++ api/src/main/java/com/qingketang/graphql/ClassroomController.java
@@ -1,8 +1,10 @@
 package com.qingketang.graphql;
 
+import com.qingketang.biz.classroom.dto.ClassroomCreateParams;
 import com.qingketang.biz.classroom.dto.ClassroomDTO;
 import com.qingketang.biz.classroom.service.ClassroomService;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -21,5 +23,10 @@ public class ClassroomController {
     @QueryMapping
     public Optional<ClassroomDTO> classroom(@Argument Long id) {
         return classroomService.get(id);
+    }
+
+    @MutationMapping
+    public ClassroomDTO createClassroom(@Argument ClassroomCreateParams params) {
+        return classroomService.create(params);
     }
 }
