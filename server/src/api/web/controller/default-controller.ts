ae3c0f700af341598b510ff7fa928bf464ac8b32
L1 L31 <wellming.li@howzhi.com>
2021-12-01 01:06:55 +0800

    chore: add typeorm

--- server/src/api/web/controller/default-controller.ts
+++ server/src/api/web/controller/default-controller.ts
@@ -1,5 +1,13 @@
 import {Context} from "koa";
+import {getRepository} from "typeorm";
+import {User} from "../../../biz/user/entity/User";
 
-export function index(ctx: Context) {
+export async function index(ctx: Context) {
     ctx.body = "hello web api";
+
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne(1);
+
+    console.log(user);
+
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,31 @@
+import "reflect-metadata";
+import {createConnection, Connection} from "typeorm";
 import Koa from "koa";
 import {router as webRouter} from "./api/web/router";
 
-const app = new Koa();
+async function main() {
+    await createConnection({
+        type: "mysql",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT as unknown as number,
+        username: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
+        charset: "utf8mb4",
+        entities: ["src/biz/**/entity/*.ts"]
+    });
 
-app.use(webRouter.routes()).use(webRouter.allowedMethods());
+    const app = new Koa();
 
-console.log(`Listen port ${process.env.PORT}`);
+    app.use(webRouter.routes()).use(webRouter.allowedMethods());
 
-app.listen(process.env.PORT);
+    console.log(`Listen port ${process.env.PORT}`);
+
+    app.listen(process.env.PORT);
+}
+
+main().then();
+
+
+
+
